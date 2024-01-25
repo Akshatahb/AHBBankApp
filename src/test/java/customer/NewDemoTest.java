0ba03b80edb69d25d8e64affc4bd63ec6bfc503e
HP <HP@DESKTOP-DLIAV7S>
2024-01-25 11:14:01 +0530

    made changes created new account

--- src/test/java/customer/NewDemoTest.java
+++ src/test/java/customer/NewDemoTest.java
@@ -7,12 +7,14 @@ public class NewDemoTest {
 	public void method1Test()
 	{
 		System.out.println("------Test 1------");
+		System.out.println("Akshata");
 	}
 	
 	@Test
 	public void method2Test()
 	{
 		System.out.println("------Test 2------");
+		System.out.println("Akshata");
 	}
 
 }
---
+++ src/test/java/customer/createATest.java
@@ -0,0 +1,12 @@
+package customer;
+
+import org.testng.annotations.Test;
+
+public class createATest {
+	@Test
+	public void createNewAccTest() {
+	
+		System.out.println("-----------Akshata Account created --------");
+	}
+
+}
