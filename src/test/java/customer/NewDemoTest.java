5d53067c567f1067e5531b9c8834e3a7d6078e10
HP <HP@DESKTOP-DLIAV7S>
2024-01-25 11:00:25 +0530

    New Bank Project 25/1/24

---
+++ src/test/java/customer/NewDemoTest.java
@@ -0,0 +1,20 @@
+package customer;
+
+import org.testng.annotations.Test;
+
+public class NewDemoTest {
+	@Test
+	public void method1Test()
+	{
+		System.out.println("------Test 1------");
+	}
+	
+	@Test
+	public void method2Test()
+	{
+		System.out.println("------Test 2------");
+	}
+
+}
+
+
---
+++ src/test/java/customer/NewSampleTest.java
@@ -0,0 +1,19 @@
+package customer;
+
+import org.testng.annotations.Test;
+
+public class NewSampleTest {
+
+    @Test
+	public void method3Test()
+	{
+		System.out.println("------Test 3------");
+	}
+
+    @Test
+	public void method4Test()
+	{
+		System.out.println("------Test 4------");
+	}
+
+}
