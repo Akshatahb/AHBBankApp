2ab8d7cddee9c6499bae242f3e7421c03933f29f
lenovo <lenovo@DESKTOP-RA7U0ME>
2024-01-25 11:23:24 +0530

    Added new class

---
+++ src/test/java/customer/AddedNewTest.java
@@ -0,0 +1,14 @@
+package customer;
+
+import org.testng.annotations.Test;
+
+public class AddedNewTest {
+	
+	@Test
+	public void runNewclsTest()
+	{
+		System.out.println("added new class");
+	}
+	
+
+}
