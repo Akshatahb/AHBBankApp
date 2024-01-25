44297ea84ff7a87d83c6e8e441149d27fefdaeaf
lenovo <lenovo@DESKTOP-RA7U0ME>
2024-01-25 11:40:15 +0530

    added new 11:40

--- src/test/java/customer/NewDemoTest.java
+++ src/test/java/customer/NewDemoTest.java
@@ -7,12 +7,18 @@ public class NewDemoTest {
 	public void method1Test()
 	{
 		System.out.println("------Test 1------");
+		System.out.println("Akshata");
 	}
 	
 	@Test
 	public void method2Test()
 	{
 		System.out.println("------Test 2------");
+		System.out.println("Akshata");
+		System.out.println("Akshay");
+		System.out.println("hello");
+		System.out.println("new branch");
+		
 	}
 
 }
